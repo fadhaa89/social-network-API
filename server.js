9b426aafeb7fb5410d75effdaf6ad7a5801a71d3
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-06-07 00:17:55 -0500

    mongoose.connect is updated

--- server.js
+++ server.js
@@ -2,4 +2,26 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
+//const PORT = process.env.PORT || 3000;
 const PORT = process.env.PORT || 3001;
+//mongoose.set("debug", true);
+
+//app.listen(PORT, () => console.log(`Successfully Connected on localhost:${PORT}`));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+//routes//
+app.use(require("./routes"));
+//mongodb://localhost/social-network-API//
+mongoose.connect(
+	process.env.MONGODB_URI || "mongodb://localhost/social-network-API",
+	{
+		useFindAndModify: false,
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}
+);
+
+mongoose.set("debug", true);
+
+app.listen(PORT, () => console.log(`Successfully Connected on localhost:${PORT}`));
