4aa094e0c10783335bc1318fcf2f70e7d8ee9b6b
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-06-11 22:25:23 -0500

    tests are updated

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
-const apiRoutes = require("./api");
-
-router.use("/api", apiRoutes);
+const apiRoutes = require("./api")
+router.use("/api",apiRoutes);
 
 router.use((req, res) => {
 	res.status(404).send("Cant go there!");
